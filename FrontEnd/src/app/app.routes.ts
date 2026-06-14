3dd4c8823cc0a3738eb082ce159471122adde9ed
Federico Balbuena <federico.balbuena@ifts24.edu.ar>
2026-06-14 17:07:05 -0300

    Agrego rutas de la app

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'products',
+        loadComponent: () =>
+            import('./pages/products/products.component')
+                .then(m => m.ProductsComponent)
+    },
+    {
+        path: 'products/create',
+        loadComponent: () =>
+            import('./pages/product-create/product-create.component')
+                .then(m => m.ProductCreateComponent)
+    },
+    {
+        path: 'products/:id',
+        loadComponent: () =>
+            import('./pages/product-detail/product-detail.component')
+                .then(m => m.ProductDetailComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () =>
+            import('./pages/contact/contact.component')
+                .then(m => m.ContactComponent)
+    },
+    {
+        path: 'products/:id/update',
+        loadComponent: () =>
+            import('./pages/product-update/product-update.component')
+                .then(m => m.ProductUpdateComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
